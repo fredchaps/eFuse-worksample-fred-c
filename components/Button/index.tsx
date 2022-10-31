f9a8ea71f6efe39afead4a6294bb5be82ef71a20
fredchaps <areinei123@gmail.com>
2022-10-31 10:03:37 -0500

    feat: add post submit button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,13 +1,18 @@
+import classnames from "classnames";
 import { ReactNode } from "react";
 import buttonStyles from "./button.module.css";
 
 type Props = {
   children: ReactNode;
   onClick: (e) => void;
+  classNames?: string;
 };
 
-const Button: React.FC<Props> = ({ children, onClick }) => (
-  <button className={buttonStyles.button} onClick={onClick}>
+const Button: React.FC<Props> = ({ children, classNames, onClick }) => (
+  <button
+    className={classnames(buttonStyles.button, classNames)}
+    onClick={onClick}
+  >
     {children}
   </button>
 );
