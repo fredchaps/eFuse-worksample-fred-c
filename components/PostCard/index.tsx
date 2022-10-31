eaf81d06efe5f39058b5d5949a6975b15df6e780
fredchaps <areinei123@gmail.com>
2022-10-31 10:04:08 -0500

    feat: add post submit component

--- components/PostCard/index.tsx
+++ components/PostCard/index.tsx
@@ -1,14 +1,27 @@
+import { Post } from "../../state/posts";
+import postStyles from "./postStyles.module.css";
 import Card from "../Card";
 import { CommentInput } from "../Inputs";
 import InteractionBar from "../InteractionBar";
 import UserHeader from "../UserHeader";
 
-const PostCard = ({ post }) => {
+type Props = {
+  post: Post;
+};
+
+const PostCard: React.FC<Props> = ({ post }) => {
+  const { createdAt, text, hypes, comments, shares, views, id } = post;
   return (
     <Card size="large">
-      <UserHeader />
-      <div>{post.text}</div>
-      <InteractionBar />
+      <UserHeader size="large" postDate={createdAt} />
+      <div className={postStyles.postContent}>{text}</div>
+      <InteractionBar
+        hypes={hypes}
+        comments={comments}
+        shares={shares}
+        views={views}
+        id={id}
+      />
       <CommentInput />
     </Card>
   );
